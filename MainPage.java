df2ed914c807dd6441ebadd2338e4932100c8b92
Onur Yıldırım <onur.yildirim.2017@ogr.iu.edu.tr>
2022-02-19 00:21:38 +0300

    Create MainPage.java

---
+++ MainPage.java
@@ -0,0 +1,10 @@
+public class MainPage {
+
+	public static void main(String[] args) {
+  
+		//Algorithm to find negative and positive numbers
+		Algorithm algorithm = new Algorithm();
+		algorithm.negAndPozFind();	
+    
+	}
+}
